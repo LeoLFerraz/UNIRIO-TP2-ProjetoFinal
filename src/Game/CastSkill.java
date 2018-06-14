3433f7369f100b2eb9368ebb6388c8816ff18600
MatheusOliveira1202 <matolisou@gmail.com>
2018-06-14 17:56:22 -0300

    Some codes changed

--- src/Game/CastSkill.java
+++ src/Game/CastSkill.java
@@ -2,8 +2,12 @@ package Game;
 
 public interface CastSkill {
 	
-	public static void castSkill(int skillNumber) 
+	/*public static void castSkill(int skillNumber, Actor caster, Actor target) 
 	{
-		
+		Skill.values()[skillNumber - 1].cast(caster, target);
+	}*/
+	public static void castSkill(Skill skillNumber, Actor caster, Actor target) 
+	{
+		skillNumber.cast(caster, target);
 	}
 }
--- src/Game/Skill.java
+++ src/Game/Skill.java
@@ -6,7 +6,27 @@ public enum Skill {
 	FIREBALL(0, 50, 0, 0, 2),
 	EARTHQUAKE(0, 100, 0, 0, 2),
 	MAGICSHIELD(1, 70, 0, 2, 0),
-	MAGICWAVE(0, 200, 0, 0, 3); 
+	MAGICWAVE(0, 200, 0, 0, 3), 
+	
+	//Warrior's skills
+	SWORDATTACK(0, 45, 2, 0, 0),
+	MORTALASSAULT(0, 120, 2, 0, 0),
+	BIGSHIELD(1, 100, 0, 0, 0),
+	TWOHANDEDSWORDATTACK(0, 200, 3, 0, 0),
+	
+	//Thief's skills
+	STEALTHASSAULT(0, 60, 0, 2, 0),
+	STEALINGMOVE(0, 100, 0, 2, 0),
+	MASTEREVASIVE(1, 100, 0, 2, 0),
+	MASSIVEQUICKATTACKS(0, 200, 0, 3, 0),
+	
+	//Archer's skills
+	FIREARROW(0, 70, 1, 1, 1),
+	ICEARROW(0, 100, 1, 1, 1),
+	RUN(1, 90, 1, 1, 1),
+	ALMIGHTYARROW(0, 180, 1, 1, 1);
+	
+	
 	
 	private int skillType;
 	private int baseValue;
@@ -69,9 +89,9 @@ public enum Skill {
 		{
 			target.setHp(target.getHp() - Math.max(finalDamage - target.getEndurance(), 0));
 		}
-		else if(this.skillType == SkillType.DEFENSIVE.getType())
+		/*else if(this.skillType == SkillType.DEFENSIVE.getType())
 		{
 			
-		}
+		}*/
 	}
 }
