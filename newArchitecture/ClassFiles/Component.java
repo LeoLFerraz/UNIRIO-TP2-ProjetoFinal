859ced00361d8b02295cb12877216a08b3ca380b
LeoLFerraz <36350448+LeoLFerraz@users.noreply.github.com>
2018-06-26 11:06:33 -0300

    Create Component.java

---
+++ newArchitecture/ClassFiles/Component.java
@@ -0,0 +1,21 @@
+package com.leonardo.ferraz.ecslib;
+
+public class Component {
+	// Attributes:
+	private final String name;
+	
+	// Constructors:
+	Component(String newName) {
+		this.name = newName;
+	}
+	
+	// Getters:
+	public String getName() {
+		return this.name;
+	}
+	
+	// Setters:
+	
+	// Methods:
+
+}
